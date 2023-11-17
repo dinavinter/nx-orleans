e3d2aff7daed212b397f1a52913cbc3ca29993c7
dina <dina.vintr@gmail.com>
2023-11-17 22:31:46 +0200

    api

---
+++ apps/api/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using static WebApiConfigurator;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseOrleans(new SiloConfigurator().ConfigurationFunc);
+
+builder.Services.AddControllers();
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+builder.Services.AddApiServices();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseHealthChecks("/health",
+  new HealthCheckOptions()
+  {
+    AllowCachingResponses = false,
+    ResultStatusCodes = healthResultStatusCodes
+  });
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
